d8909ab2cf3f20b1986d0ee8427ea7cfe7cd8aee
lucaswmolin <lwmolin@gmail.com>
2021-07-10 23:12:02 -0300

    Hello World

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace CombinationsTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbValues = new System.Windows.Forms.Label();
+            this.lbCategories = new System.Windows.Forms.Label();
+            this.lbFinalSum = new System.Windows.Forms.Label();
+            this.tbValues = new System.Windows.Forms.TextBox();
+            this.tbCategories = new System.Windows.Forms.TextBox();
+            this.tbFinalSum = new System.Windows.Forms.TextBox();
+            this.btCalculate = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.lbResultCount = new System.Windows.Forms.Label();
+            this.btClean = new System.Windows.Forms.Button();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.lbCombinationsTime = new System.Windows.Forms.Label();
+            this.lbWritingTime = new System.Windows.Forms.Label();
+            this.lbThreads = new System.Windows.Forms.Label();
+            this.lbAllCombinations = new System.Windows.Forms.Label();
+            this.cbThreads = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbValues
+            // 
+            this.lbValues.AutoSize = true;
+            this.lbValues.Location = new System.Drawing.Point(12, 9);
+            this.lbValues.Name = "lbValues";
+            this.lbValues.Size = new System.Drawing.Size(169, 15);
+            this.lbValues.TabIndex = 0;
+            this.lbValues.Text = "Informe o conjunto de valores:";
+            // 
+            // lbCategories
+            // 
+            this.lbCategories.AutoSize = true;
+            this.lbCategories.Location = new System.Drawing.Point(12, 53);
+            this.lbCategories.Name = "lbCategories";
+            this.lbCategories.Size = new System.Drawing.Size(254, 15);
+            this.lbCategories.TabIndex = 1;
+            this.lbCategories.Text = "Informe a categoria a qual cada valor pertence:";
+            // 
+            // lbFinalSum
+            // 
+            this.lbFinalSum.AutoSize = true;
+            this.lbFinalSum.Location = new System.Drawing.Point(804, 9);
+            this.lbFinalSum.Name = "lbFinalSum";
+            this.lbFinalSum.Size = new System.Drawing.Size(118, 15);
+            this.lbFinalSum.TabIndex = 2;
+            this.lbFinalSum.Text = "Informe o valor total:";
+            // 
+            // tbValues
+            // 
+            this.tbValues.Location = new System.Drawing.Point(12, 27);
+            this.tbValues.Name = "tbValues";
+            this.tbValues.Size = new System.Drawing.Size(786, 23);
+            this.tbValues.TabIndex = 3;
+            // 
+            // tbCategories
+            // 
+            this.tbCategories.Location = new System.Drawing.Point(12, 71);
+            this.tbCategories.Name = "tbCategories";
+            this.tbCategories.Size = new System.Drawing.Size(786, 23);
+            this.tbCategories.TabIndex = 4;
+            // 
+            // tbFinalSum
+            // 
+            this.tbFinalSum.Location = new System.Drawing.Point(804, 27);
+            this.tbFinalSum.Name = "tbFinalSum";
+            this.tbFinalSum.Size = new System.Drawing.Size(183, 23);
+            this.tbFinalSum.TabIndex = 5;
+            // 
+            // btCalculate
+            // 
+            this.btCalculate.Location = new System.Drawing.Point(899, 100);
+            this.btCalculate.Name = "btCalculate";
+            this.btCalculate.Size = new System.Drawing.Size(88, 24);
+            this.btCalculate.TabIndex = 6;
+            this.btCalculate.Text = "Calcular";
+            this.btCalculate.UseVisualStyleBackColor = true;
+            this.btCalculate.Click += new System.EventHandler(this.btCalculate_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.ItemHeight = 15;
+            this.lbResult.Location = new System.Drawing.Point(12, 131);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(975, 319);
+            this.lbResult.TabIndex = 7;
+            // 
+            // lbResultCount
+            // 
+            this.lbResultCount.AutoSize = true;
+            this.lbResultCount.Location = new System.Drawing.Point(956, 453);
+            this.lbResultCount.Name = "lbResultCount";
+            this.lbResultCount.Size = new System.Drawing.Size(31, 15);
+            this.lbResultCount.TabIndex = 9;
+            this.lbResultCount.Text = "0000";
+            // 
+            // btClean
+            // 
+            this.btClean.Location = new System.Drawing.Point(804, 100);
+            this.btClean.Name = "btClean";
+            this.btClean.Size = new System.Drawing.Size(89, 24);
+            this.btClean.TabIndex = 10;
+            this.btClean.Text = "Apagar";
+            this.btClean.UseVisualStyleBackColor = true;
+            this.btClean.Click += new System.EventHandler(this.btClean_Click);
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(199, 453);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(76, 15);
+            this.lbTime.TabIndex = 11;
+            this.lbTime.Text = "T: 00:00:00:00";
+            // 
+            // lbCombinationsTime
+            // 
+            this.lbCombinationsTime.AutoSize = true;
+            this.lbCombinationsTime.Location = new System.Drawing.Point(12, 453);
+            this.lbCombinationsTime.Name = "lbCombinationsTime";
+            this.lbCombinationsTime.Size = new System.Drawing.Size(78, 15);
+            this.lbCombinationsTime.TabIndex = 13;
+            this.lbCombinationsTime.Text = "C: 00:00:00:00";
+            // 
+            // lbWritingTime
+            // 
+            this.lbWritingTime.AutoSize = true;
+            this.lbWritingTime.Location = new System.Drawing.Point(105, 453);
+            this.lbWritingTime.Name = "lbWritingTime";
+            this.lbWritingTime.Size = new System.Drawing.Size(76, 15);
+            this.lbWritingTime.TabIndex = 15;
+            this.lbWritingTime.Text = "E: 00:00:00:00";
+            // 
+            // lbThreads
+            // 
+            this.lbThreads.AutoSize = true;
+            this.lbThreads.Location = new System.Drawing.Point(804, 53);
+            this.lbThreads.Name = "lbThreads";
+            this.lbThreads.Size = new System.Drawing.Size(130, 15);
+            this.lbThreads.TabIndex = 16;
+            this.lbThreads.Text = "Quantidade de threads:";
+            // 
+            // lbAllCombinations
+            // 
+            this.lbAllCombinations.AutoSize = true;
+            this.lbAllCombinations.Location = new System.Drawing.Point(12, 106);
+            this.lbAllCombinations.Name = "lbAllCombinations";
+            this.lbAllCombinations.Size = new System.Drawing.Size(79, 15);
+            this.lbAllCombinations.TabIndex = 18;
+            this.lbAllCombinations.Text = "000000000000";
+            // 
+            // cbThreads
+            // 
+            this.cbThreads.FormattingEnabled = true;
+            this.cbThreads.Location = new System.Drawing.Point(804, 71);
+            this.cbThreads.Name = "cbThreads";
+            this.cbThreads.Size = new System.Drawing.Size(183, 23);
+            this.cbThreads.TabIndex = 19;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 473);
+            this.Controls.Add(this.cbThreads);
+            this.Controls.Add(this.lbAllCombinations);
+            this.Controls.Add(this.lbThreads);
+            this.Controls.Add(this.lbWritingTime);
+            this.Controls.Add(this.lbCombinationsTime);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.btClean);
+            this.Controls.Add(this.lbResultCount);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btCalculate);
+            this.Controls.Add(this.tbFinalSum);
+            this.Controls.Add(this.tbCategories);
+            this.Controls.Add(this.tbValues);
+            this.Controls.Add(this.lbFinalSum);
+            this.Controls.Add(this.lbCategories);
+            this.Controls.Add(this.lbValues);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Combinations Tool by LWMolin (v. 2021.07.10.1)";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbValues;
+        private System.Windows.Forms.Label lbCategories;
+        private System.Windows.Forms.Label lbFinalSum;
+        private System.Windows.Forms.TextBox tbValues;
+        private System.Windows.Forms.TextBox tbCategories;
+        private System.Windows.Forms.TextBox tbFinalSum;
+        private System.Windows.Forms.Button btCalculate;
+        private System.Windows.Forms.ListBox lbResult;
+        private System.Windows.Forms.Label lbResultCount;
+        private System.Windows.Forms.Button btClean;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label lbCombinationsTime;
+        private System.Windows.Forms.Label lbWritingTime;
+        private System.Windows.Forms.Label lbThreads;
+        private System.Windows.Forms.Label lbAllCombinations;
+        private System.Windows.Forms.ComboBox cbThreads;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombinationsTool
+{
+    public partial class MainForm : Form
+    {
+        private static Semaphore semaphore1;
+        private static Semaphore semaphore2;
+
+        private static List<List<float>> allCombinations = new List<List<float>>();
+        private static List<List<string>> lResult = new List<List<string>>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void btCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clean();
+
+                int cpu = Int32.Parse(cbThreads.SelectedItem.ToString());
+
+                Run(tbValues.Text, tbFinalSum.Text, tbCategories.Text, cpu);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }       
+        }
+
+        void btClean_Click(object sender, EventArgs e)
+        {
+            cleanAll();
+        }
+
+        void cleanAll()
+        {
+            tbValues.Clear();
+            tbCategories.Clear();
+            tbFinalSum.Clear();
+            lbResult.Items.Clear();
+            lbResultCount.Text = String.Empty;
+            lbAllCombinations.Text = String.Empty;
+            lbCombinationsTime.Text = String.Empty;
+            lbWritingTime.Text = String.Empty;
+            lbTime.Text = String.Empty;
+            tbValues.Focus();
+
+            allCombinations.Clear();
+            lResult.Clear();
+        }
+
+        void clean()
+        {
+            lbResultCount.Text = String.Empty;
+            lbAllCombinations.Text = String.Empty;
+            lbCombinationsTime.Text = String.Empty;
+            lbWritingTime.Text = String.Empty;
+            lbTime.Text = String.Empty;
+            lbResult.Items.Clear();
+            allCombinations.Clear();
+            lResult.Clear();
+        }
+
+        List<string> FormatList(List<float> combination, List<float> allValues, List<string> categories)
+        {
+            List<string> result = new List<string>();
+
+            string fdValues = "In: ";
+            foreach (float f in combination)
+            {
+                fdValues += string.Format("{0:N2}", f) + " (" + FindCategory(f, allValues, categories) + "), ";
+            }
+
+            fdValues = fdValues.Substring(0, fdValues.Length - 2);
+
+            result.Add(fdValues);
+
+            string nfValues = "Out: ";
+            for (int i = 0; i < allValues.Count; i++)
+            {
+                bool found = false;
+                foreach (float f in combination)
+                {
+                    if (allValues[i] == f)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    nfValues += string.Format("{0:N2}", allValues[i]) + " (" + FindCategory(allValues[i], allValues, categories) + "), ";
+                }
+            }
+
+            nfValues = nfValues.Substring(0, nfValues.Length - 2);
+
+            result.Add(nfValues);
+
+            result.Add(String.Empty);
+
+            return result;
+        }
+
+        string FindCategory(float value, List<float> values, List<string> categories)
+        {
+            if (categories.Count > 1) {
+                for (int i = 0; i < values.Count; i++)
+                {
+                    if (value == values[i])
+                    {
+                        return categories[i];
+                    }
+                }
+            }
+
+            return String.Empty;
+        }
+
+        void Calculate(List<List<float>> combinations, List<float> values, List<string> categories, float total)
+        {
+            for (int i = 0; i < combinations.Count; i++)
+            {
+                float vSum = 0;
+                foreach (float n in combinations[i])
+                {
+                    vSum += n;
+                }
+
+                if (Math.Round(vSum, 2) == Math.Round(total, 2))
+                {
+                    List<string> combination = FormatList(combinations[i], values, categories);
+
+                    semaphore2.WaitOne();
+
+                    lResult.Add(combination);
+
+                    semaphore2.Release();
+                }
+            }
+        }
+
+        void GenerateCombinations(float[] arr, int n, int r, int index, float[] data, int i)
+        {
+            if (index == r)
+            {
+                List<float> combination = new List<float>();
+                for (int j = 0; j < r; j++)
+                {
+                    combination.Add(data[j]);
+                }
+
+                semaphore1.WaitOne();
+
+                allCombinations.Add(combination);
+
+                semaphore1.Release();
+
+                return;
+            }
+
+            if (i >= n)
+            {
+                return;
+            }
+
+            data[index] = arr[i];
+
+            GenerateCombinations(arr, n, r, index + 1, data, i + 1);
+
+            GenerateCombinations(arr, n, r, index, data, i + 1);
+        }
+
+        void GetCombinations(float[] arr, int n, List<int> lSize)
+        {
+            foreach (int r in lSize)
+            {
+                float[] data = new float[r];
+
+                GenerateCombinations(arr, n, r, 0, data, 0);
+            }
+        }
+
+        void Run(string values, string finalSum, string categories, int cpu)
+        {       
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string[] strValues = values.Split(",");
+            string[] strCategories = categories.Split(",");
+            float fFSum = float.Parse(finalSum);
+
+            List<float> fValues = new List<float>();
+            for (int i = 0; i < strValues.Length; i++)
+            {
+                fValues.Add(float.Parse(strValues[i]) + ((float)i + 1) / 100000);
+            }
+
+            List<string> fCategories = new List<string>();
+            for (int i = 0; i < strCategories.Length; i++)
+            {
+                fCategories.Add(strCategories[i]);
+            }
+
+
+            var combinationsClock = new Stopwatch();
+            combinationsClock.Start();
+
+            semaphore1 = new Semaphore(1, 1);
+
+            float[] arr = fValues.ToArray();
+            int n = arr.Length;
+
+            List<Thread> kThread = new List<Thread>();
+            for (int i = 1; i <= cpu; i++)
+            {
+                List<int> lSize = new List<int>();
+
+                int id = i;
+                while (id <= fValues.Count)
+                {
+                    lSize.Add(id);
+                    id += cpu;
+                }
+
+                Thread thread = new Thread(() => GetCombinations(arr, n, lSize));
+                kThread.Add(thread);
+            }
+
+            foreach (Thread t in kThread)
+            {
+                t.Start();
+            }
+
+            foreach (Thread t in kThread)
+            {
+                t.Join();
+            }
+
+            lbAllCombinations.Text = allCombinations.Count.ToString();
+
+            var combinations = allCombinations;
+
+            int interval = combinations.Count / cpu;
+            int residue = combinations.Count % cpu;
+
+            int mn = 0;
+            int mx = interval;
+
+            semaphore2 = new Semaphore(1, 1);
+
+            List<Thread> lThread = new List<Thread>();
+            for (int i = 0; i < cpu; i++)
+            {
+                if (i == (cpu - 1))
+                {
+                    mx += residue;
+                }
+
+                List<List<float>> subCombinations = new List<List<float>>();
+                for (int j = mn; j < mx; j++)
+                {
+                    subCombinations.Add(combinations[j]);
+                }
+
+                mn += interval;
+                mx += interval;
+
+                Thread thread = new Thread(() => Calculate(subCombinations, fValues, fCategories, fFSum));
+                lThread.Add(thread);
+            }
+
+            foreach (Thread t in lThread)
+            {
+                t.Start();
+            }
+
+            foreach (Thread t in lThread)
+            {
+                t.Join();
+            }
+
+            combinationsClock.Stop();
+
+            TimeSpan combinationsTime = TimeSpan.FromMilliseconds(combinationsClock.ElapsedMilliseconds);
+            lbCombinationsTime.Text = "C: " + combinationsTime.ToString(@"hh\:mm\:ss\:ms");
+
+
+            var writingClock = new Stopwatch();
+            writingClock.Start();
+
+            if (lResult.Count > 0) {
+                foreach (List<string> l in lResult)
+                {
+                    foreach (string sl in l)
+                    {
+                        lbResult.Items.Add(sl);
+                    }
+                }
+            } else
+            {
+                lbResult.Items.Add("Nenhuma combinação foi encontrada.");
+            }
+
+            writingClock.Stop();
+
+            TimeSpan writingTime = TimeSpan.FromMilliseconds(writingClock.ElapsedMilliseconds);
+            lbWritingTime.Text = "E: " + writingTime.ToString(@"hh\:mm\:ss\:ms");
+
+
+            stopwatch.Stop();
+
+            TimeSpan ts = TimeSpan.FromMilliseconds(stopwatch.ElapsedMilliseconds);
+            lbTime.Text = "T: " + ts.ToString(@"hh\:mm\:ss\:ms");
+
+            if (lResult.Count <= 999){
+                lbResultCount.Text = String.Format("{0:0000}", lResult.Count);
+            } else
+            {
+                lbResultCount.Text = "+999";
+            }
+        }
+
+        void MainForm_Load(object sender, EventArgs e)
+        {
+            cleanAll();
+
+            for (int i = 1; i <= Environment.ProcessorCount; i++)
+            {
+                cbThreads.Items.Add(i.ToString());
+            }
+        }
+    }
+}
